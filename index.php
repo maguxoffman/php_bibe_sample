e0d8843fa77d329e5151afa6caca48340a83a613
offman <maguxoffman@gmail.com>
2025-12-24 15:06:03 +0900

    feat: Add security-themed favicon

--- index.php
+++ index.php
@@ -66,6 +66,7 @@ if (file_exists($configFile)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XDOS Configuration Editor</title>
+    <link rel="icon" type="image/png" href="favicon.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
