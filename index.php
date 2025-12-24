4d0dd6f92e39171fa52b2568e3bd9c90746c3086
offman <maguxoffman@gmail.com>
2025-12-24 15:08:45 +0900

    refactor: Rename favicon.png to favicon.ico

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if (file_exists($configFile)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XDOS Configuration Editor</title>
-    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
